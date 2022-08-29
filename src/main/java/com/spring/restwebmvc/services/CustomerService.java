ef859a0ef48dc759652e9978d5442520c5f0d86f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-29 20:49:01 +0530

    add customer service class

---
+++ src/main/java/com/spring/restwebmvc/services/CustomerService.java
@@ -0,0 +1,11 @@
+package com.spring.restwebmvc.services;
+
+import com.spring.restwebmvc.api.v1.model.CustomerDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<CustomerDTO> getAllCustomers();
+
+    CustomerDTO getCustomerById(Long id);
+}
