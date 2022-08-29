9f47fb4b23186bba741860ac8d3f4b69c28fbb93
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-30 01:11:40 +0530

    add lombok annotations

--- src/main/java/com/spring/restwebmvc/api/v1/model/CustomerDTO.java
+++ src/main/java/com/spring/restwebmvc/api/v1/model/CustomerDTO.java
@@ -1,8 +1,12 @@
 package com.spring.restwebmvc.api.v1.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
     private String firstName;
     private String lastName;
