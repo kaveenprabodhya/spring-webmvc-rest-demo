3c5e7b710178a43a8f639dc296e815421cfeecc0
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-29 14:03:19 +0530

    add CategoryMapper

---
+++ src/main/java/com/spring/restwebmvc/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.spring.restwebmvc.api.v1.mapper;
+
+import com.spring.restwebmvc.api.v1.model.CategoryDTO;
+import com.spring.restwebmvc.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
