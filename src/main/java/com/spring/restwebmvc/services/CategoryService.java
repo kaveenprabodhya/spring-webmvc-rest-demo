62d315946b6fecb7cbc7f571415c8a9044448f83
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-29 14:14:22 +0530

    add methods in CategoryService

--- src/main/java/com/spring/restwebmvc/services/CategoryService.java
+++ src/main/java/com/spring/restwebmvc/services/CategoryService.java
@@ -1,4 +1,11 @@
 package com.spring.restwebmvc.services;
 
+import com.spring.restwebmvc.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
 public interface CategoryService {
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
 }
