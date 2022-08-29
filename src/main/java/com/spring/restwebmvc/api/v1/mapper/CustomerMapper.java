a8a02a5c30833494c9169b8bd8bb428088f4de98
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-29 20:48:39 +0530

    add customer mapper class

---
+++ src/main/java/com/spring/restwebmvc/api/v1/mapper/CustomerMapper.java
@@ -0,0 +1,12 @@
+package com.spring.restwebmvc.api.v1.mapper;
+
+import com.spring.restwebmvc.api.v1.model.CustomerDTO;
+import com.spring.restwebmvc.domain.Customer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface CustomerMapper {
+    CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
+    CustomerDTO customerToCustomerDTO(Customer customer);
+}
