08be33d1755b39d7b287a24ce2b56bbeabd6212a
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-29 20:50:18 +0530

    add listCustomers() to Bootstrap

--- src/main/java/com/spring/restwebmvc/bootstrap/Bootstrap.java
+++ src/main/java/com/spring/restwebmvc/bootstrap/Bootstrap.java
@@ -1,20 +1,29 @@
 package com.spring.restwebmvc.bootstrap;
 
 import com.spring.restwebmvc.domain.Category;
+import com.spring.restwebmvc.domain.Customer;
 import com.spring.restwebmvc.repository.CategoryRepository;
+import com.spring.restwebmvc.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        loadCategories();
+        loadCustomers();
+    }
+
+    private void loadCategories() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -37,5 +46,21 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(nuts);
 
         System.out.println("Data Loaded = "+categoryRepository.count());
+    }
+
+    private void loadCustomers(){
+        Customer customer1 = new Customer();
+        customer1.setId(1L);
+        customer1.setFirstName("John");
+        customer1.setLastName("Doe");
+        customerRepository.save(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(2L);
+        customer2.setFirstName("kaveen");
+        customer2.setLastName("Thivanka");
+        customerRepository.save(customer2);
+
+        System.out.println("Customers Loaded = "+customerRepository.count());
     }
 }
