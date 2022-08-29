55b50f91c8e235363a3a371bac21d34a60f25c34
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-29 14:16:43 +0530

    impl CategoryServiceImpl

---
+++ src/main/java/com/spring/restwebmvc/services/CategoryServiceImpl.java
@@ -0,0 +1,17 @@
+package com.spring.restwebmvc.services;
+
+import com.spring.restwebmvc.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+        return null;
+    }
+
+    @Override
+    public CategoryDTO getCategoryByName(String name) {
+        return null;
+    }
+}
