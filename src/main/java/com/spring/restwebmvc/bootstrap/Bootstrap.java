f85cf3de6918fbb7a1390aed12a789bd34a17019
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-30 16:46:44 +0530

    load vendors

--- src/main/java/com/spring/restwebmvc/bootstrap/Bootstrap.java
+++ src/main/java/com/spring/restwebmvc/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package com.spring.restwebmvc.bootstrap;
 
 import com.spring.restwebmvc.domain.Category;
 import com.spring.restwebmvc.domain.Customer;
+import com.spring.restwebmvc.domain.Vendor;
 import com.spring.restwebmvc.repository.CategoryRepository;
 import com.spring.restwebmvc.repository.CustomerRepository;
+import com.spring.restwebmvc.repository.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +13,31 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
     private CategoryRepository categoryRepository;
     private CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         loadCategories();
         loadCustomers();
+        loadVendors();
+    }
+
+    private void loadVendors() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Vendor 1");
+        vendorRepository.save(vendor1);
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Vendor 2");
+        vendorRepository.save(vendor2);
+
     }
 
     private void loadCategories() {
