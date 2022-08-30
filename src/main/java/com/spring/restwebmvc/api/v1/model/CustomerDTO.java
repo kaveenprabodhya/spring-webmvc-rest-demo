43087fef5c13fd85ba461e8f967a8713d1a803c8
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-30 21:35:25 +0530

    change api model property name to value

--- src/main/java/com/spring/restwebmvc/api/v1/model/CustomerDTO.java
+++ src/main/java/com/spring/restwebmvc/api/v1/model/CustomerDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDTO {
-    @ApiModelProperty(name = "Firstname of Customer", required = true)
+    @ApiModelProperty(value = "Firstname of Customer", required = true)
     private String firstname;
     private String lastname;
     @JsonProperty("customer_url")
